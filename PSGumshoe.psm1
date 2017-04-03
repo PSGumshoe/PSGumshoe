0a168c798440c0c7ba6fa7e78139c6b912e4289e
Matt Johnson <mjohnson@mwjcomputing.com>
2017-04-03 10:27:04 -0400

    Add Get-NamedPipe
    Added Get-NamedPipe to PSGumeshoe.psm1
    Added Get-NamedPipe to Functions to Export in PSGumshoe.psd1
    Added Get-NamedPipe.tests.ps1

--- PSGumshoe.psm1
+++ PSGumshoe.psm1
@@ -167,3 +167,37 @@ function Measure-VectorSimilarity
     ## Return the result
     [Math]::Round($dot / ($mag1 * $mag2), 3)
 }
+
+function Get-NamedPipe {
+	<#
+		.SYNOPSIS
+			Gets named pipes on local computer.
+		
+		.DESCRIPTION
+			Gets named pipes on the local computer.
+		
+		.EXAMPLE
+			PS C:\> Get-PsgNamedPipes
+		
+		.NOTES
+			Additional information about the function.
+	#>
+
+  [CmdletBinding()]
+	param ()
+	begin {
+		$PipeList = @()	
+	}
+	process{
+		$Pipes = [IO.Directory]::GetFiles('\\.\pipe\')
+		
+		foreach ($Pipe in $Pipes) {
+			$Object = New-Object -TypeName PSObject -Property (@{ 'NamedPipe' = $Pipe })
+			$PipeList += $Object
+			
+		}
+	}
+	end {
+		Write-Output -InputObject $PipeList	
+	}
+}
---
+++ tests/Get-NamedPipe.tests.ps1
@@ -0,0 +1,16 @@
+﻿Describe 'Get-PsgNamedPipe' {
+	It 'Runs without error' {
+		{ Get-PsgNamedPipe } | Should Not Throw
+	}
+	
+	It 'Finds the LSASS Pipe' {
+		$Pipes = Get-PsgNamedPipe
+		foreach ($Pipe in $Pipes) {
+			if ($Pipe -contains '\\.\pipe\lsass') {
+				$Found = $true	
+			}
+		}
+		
+		$Found | Should Be $true
+	}
+}
