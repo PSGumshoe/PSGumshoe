3d4fa64d42e0a9688b02fa706e8eeff6225b2285
Carlos Perez <carlos_perez@darkoperator.com>
2019-07-26 10:08:04 -0400

    fix path for loading of functions

--- PSGumshoe.psm1
+++ PSGumshoe.psm1
@@ -62,20 +62,20 @@
 . $PSScriptRoot\EventLog\Get-SysmonNetworkConnect.ps1
 . $PSScriptRoot\EventLog\Search-SysmonEvent.ps1
 . $PSScriptRoot\EventLog\Get-SysmonProcessActivityEvent.ps1
-. $PSScriptRoot\Search-EventLogEventData.ps1
-. $PSScriptRoot\Search-EventLogEventXML.ps1
-. $PSScriptRoot\ConvertFrom-EventLogonRecord.ps1
-. $PSScriptRoot\ConvertFrom-EventEventXMLRecord.ps1
-. $PSScriptRoot\Get-EventSystemLogon.ps1
-. $PSScriptRoot\Get-EventSystemLogoff.ps1
-. $PSScriptRoot\Get-EventTerminalLogon.ps1
-. $PSScriptRoot\Get-EventTerminalLogoff.ps1
-. $PSScriptRoot\Get-EventScheduledTaskStart.ps1
-. $PSScriptRoot\Get-EventScheduledTaskProcess.ps1
-. $PSScriptRoot\Get-EventScheduledTaskStop.ps1
-. $PSScriptRoot\Get-EventScheduledTaskComplete.ps1
-. $PSScriptRoot\Get-EventBitsTransferComplete.ps1
-. $PSScriptRoot\Get-EventBitsTransferStart.ps1
+. $PSScriptRoot\EventLog\Search-EventLogEventData.ps1
+. $PSScriptRoot\EventLog\Search-EventLogEventXML.ps1
+. $PSScriptRoot\EventLog\ConvertFrom-EventLogonRecord.ps1
+. $PSScriptRoot\EventLog\ConvertFrom-EventEventXMLRecord.ps1
+. $PSScriptRoot\EventLog\Get-EventSystemLogon.ps1
+. $PSScriptRoot\EventLog\Get-EventSystemLogoff.ps1
+. $PSScriptRoot\EventLog\Get-EventTerminalLogon.ps1
+. $PSScriptRoot\EventLog\Get-EventTerminalLogoff.ps1
+. $PSScriptRoot\EventLog\Get-EventScheduledTaskStart.ps1
+. $PSScriptRoot\EventLog\Get-EventScheduledTaskProcess.ps1
+. $PSScriptRoot\EventLog\Get-EventScheduledTaskStop.ps1
+. $PSScriptRoot\EventLog\Get-EventScheduledTaskComplete.ps1
+. $PSScriptRoot\EventLog\Get-EventBitsTransferComplete.ps1
+. $PSScriptRoot\EventLog\Get-EventBitsTransferStart.ps1
 
 
 #region PSReflect
