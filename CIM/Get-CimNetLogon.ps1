4abbe78596566477f3cabc1418220965c29e0006
Carlos Perez <carlos_perez@darkoperato.com>
2021-01-09 20:07:37 -0400

    Added hep information.

--- CIM/Get-CimNetLogon.ps1
+++ CIM/Get-CimNetLogon.ps1
@@ -1,4 +1,180 @@
 function Get-CimNetLogon {
+    <#
+    .SYNOPSIS
+        Get Netlogon cached information entries for Windows 8/2012 or above systems leveraging CIM.
+    .DESCRIPTION
+        Get Netlogon cached information entries for Windows 8/2012 or above systems leveraging CIM. When ran
+        elevated or remotely as an adminitrator y provides all cached netlogon entries if not it will return
+        information for the System account and the cuser executing the query.
+    .EXAMPLE
+        PS C:\> Get-CimNetLogon
+
+        Name            : NT AUTHORITY\SYSTEM
+        Caption         : NT AUTHORITY\SYSTEM
+        LastLogon       : 
+        Comment         : 
+        NumberOfLogons  : 
+        Privileges      : 
+        PrimaryGroupId  : 
+        ScriptPath      : 
+        PasswordExpires : 
+        PasswordAge     : 
+        UserId          : 
+        Flags           : {}
+
+        Name            : ACMELABS\cperez
+        Caption         : cperez
+        LastLogon       : 1/9/2021 3:32:14 PM
+        Comment         : 
+        NumberOfLogons  : 487
+        Privileges      : Administrator
+        PrimaryGroupId  : 513
+        ScriptPath      : 
+        PasswordExpires :
+        PasswordAge     : 2.13:07:50
+        UserId          : 1618
+        Flags           : {PasswordNotExpires, NormalAccount}
+
+
+        Running localy in a none elevated window. 
+
+    .EXAMPLE
+
+    PS C:\> Get-CimNetLogon
+
+
+    Name            : NT AUTHORITY\SYSTEM
+    Caption         : NT AUTHORITY\SYSTEM
+    LastLogon       :
+    Comment         :
+    NumberOfLogons  :
+    Privileges      :
+    PrimaryGroupId  :
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     :
+    UserId          :
+    Flags           : {}
+
+    Name            : NT AUTHORITY\LOCAL SERVICE
+    Caption         : NT AUTHORITY\LOCAL SERVICE
+    LastLogon       :
+    Comment         :
+    NumberOfLogons  :
+    Privileges      :
+    PrimaryGroupId  :
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     :
+    UserId          :
+    Flags           : {}
+
+    Name            : NT AUTHORITY\NETWORK SERVICE
+    Caption         : NT AUTHORITY\NETWORK SERVICE
+    LastLogon       :
+    Comment         :
+    NumberOfLogons  :
+    Privileges      :
+    PrimaryGroupId  :
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     :
+    UserId          :
+    Flags           : {}
+
+    Name            : CL01\admin
+    Caption         : admin
+    LastLogon       : 8/25/2019 4:12:47 PM
+    Comment         :
+    NumberOfLogons  : 16
+    Privileges      : Administrator
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 512.07:32:33
+    UserId          : 1000
+    Flags           : {NormalAccount}
+
+    Name            : ACMELABS\Distle
+    Caption         : Distle
+    LastLogon       : 5/26/2019 2:22:45 AM
+    Comment         :
+    NumberOfLogons  : 9
+    Privileges      : User
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 624.22:27:06
+    UserId          : 1164
+    Flags           : {PasswordNotExpires, NormalAccount}
+
+    Name            : ACMELABS\rubenb
+    Caption         : rubenb
+    LastLogon       : 9/8/2020 2:41:03 PM
+    Comment         :
+    NumberOfLogons  : 2
+    Privileges      : User
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 123.06:39:12
+    UserId          : 1298
+    Flags           : {PasswordNotExpires, NormalAccount}
+
+    Name            : ACMELABS\thomasb
+    Caption         : thomasb
+    LastLogon       : 1/8/2021 4:46:15 PM
+    Comment         :
+    NumberOfLogons  : 11
+    Privileges      : User
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 549.07:31:24
+    UserId          : 1314
+    Flags           : {PasswordNotExpires, NormalAccount}
+
+    Name            : ACMELABS\Stlece
+    Caption         : Stlece
+    LastLogon       : 4/10/2019 7:37:45 PM
+    Comment         :
+    NumberOfLogons  : 10
+    Privileges      : User
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 652.10:16:24
+    UserId          : 1590
+    Flags           : {PasswordNotExpires, NormalAccount}
+
+    Name            : ACMELABS\cperez
+    Caption         : cperez
+    LastLogon       : 1/9/2021 3:32:14 PM
+    Comment         :
+    NumberOfLogons  : 487
+    Privileges      : Administrator
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 2.13:13:41
+    UserId          : 1618
+    Flags           : {PasswordNotExpires, NormalAccount}
+
+    Name            : ACMELABS\Administrator
+    Caption         : Administrator
+    LastLogon       : 1/9/2021 6:24:34 PM
+    Comment         : Darkoperator.com
+    NumberOfLogons  : 782
+    Privileges      : Administrator
+    PrimaryGroupId  : 513
+    ScriptPath      :
+    PasswordExpires :
+    PasswordAge     : 512.07:24:30
+    UserId          : 500
+    Flags           : {NormalAccount}
+    
+    Running the function from an elevated prompt. 
+    #>
     [CmdletBinding()]
     param (
         # CIMSession to perform query against
@@ -14,12 +190,6 @@ function Get-CimNetLogon {
          if ($null -eq $CimSession -or $CimSession.Count -eq 0) {
             $sessop = New-CimSessionOption -Protocol Dcom
             $CimSession += New-CimSession -ComputerName $env:COMPUTERNAME -SessionOption $sessop
-        }
-
-        $privs = @{ 
-            0 ="Guess"
-            1 = "User"
-            2 = "Administrator" 
         }
 
         $flags = @{
